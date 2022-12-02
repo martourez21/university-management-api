721a82f5e441a77e78b8227563ff57818f5abe4b
martourez21 <nestorabiawuh@gmail.com>
2022-12-02 05:52:58 -0800

    added response exception handler class for department

--- src/test/java/UniversityManagementApiApplicationTests.java
+++ src/test/java/UniversityManagementApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.university.universitymanagementapi;
+package com.university.universitymanagementapi.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
